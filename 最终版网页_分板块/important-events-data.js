5bf780cc43428fa044fcce03278b8c516f1c4cb9
lyf-create <2482656716@qq.com>
2026-08-20 06:45:30 +0800

    add curated 2026 high-score important events data

---
+++ 最终版网页_分板块/important-events-data.js
@@ -0,0 +1,98 @@
+window.JJJ_IMPORTANT_EVENTS_2026 = [
+  {
+    date: '2026-05-15',
+    title: '商科高校服务京津冀协同发展合作体在津成立',
+    source: '天津市教育委员会',
+    url: 'https://jy.tj.gov.cn/JYXW/TJJY/202605/t20260519_7301588.html',
+    score: 28,
+    eventId: 'E00350'
+  },
+  {
+    date: '2026-05-19',
+    title: '京津冀特殊职业教育产教联合体成立大会召开',
+    source: '天津市教育委员会',
+    url: 'https://jy.tj.gov.cn/JYXW/TJJY/202605/t20260519_7301596.html',
+    score: 28,
+    eventId: 'E00352'
+  },
+  {
+    date: '2026-05-23',
+    title: '海淀南开雄安签署人才协同发展合作框架协议',
+    source: '人民网',
+    url: 'http://tj.people.com.cn/n2/2026/0525/c375366-41590110.html',
+    score: 23,
+    eventId: 'E00356'
+  },
+  {
+    date: '2026-06-22',
+    title: '京津冀发布九条措施支持雄安新区技能人才队伍建设',
+    source: '人力资源和社会保障部',
+    url: 'https://chinajob.mohrss.gov.cn/c/2026-06-22/560513.shtml',
+    score: 20,
+    eventId: 'E00364'
+  },
+  {
+    date: '2026-04-14',
+    title: '京津冀高校首个智能机器人服务中心在津揭牌',
+    source: '天津市教育委员会',
+    url: 'https://jy.tj.gov.cn/JYXW/TJJY/202604/t20260415_7282170.html',
+    score: 17,
+    eventId: 'E00333'
+  },
+  {
+    date: '2026-04-19',
+    title: '天津师范大学学前教育学院与雄安人才集团签署校企合作协议共建产教融合基地',
+    source: '天津市人民政府',
+    url: 'https://www.tj.gov.cn/sy/tjxw/202604/t20260419_7284032.html',
+    score: 15,
+    eventId: 'E00335'
+  },
+  {
+    date: '2026-06-03',
+    title: '2026年京津冀教育协同发展现场工作会召开',
+    source: '天津市教育委员会',
+    url: 'https://jy.tj.gov.cn/JYXW/TJJY/202606/t20260603_7311110.html',
+    score: 13,
+    eventId: 'E00359'
+  },
+  {
+    date: '2026-05-08',
+    title: '北京（顺义）宽禁带半导体联合中试平台发布',
+    source: '北京市科委、中关村管委会',
+    url: 'https://kw.beijing.gov.cn/xwdt/kcyx/xwdtyqqy/202605/t20260508_4640783.html',
+    score: 12,
+    eventId: 'E00348'
+  },
+  {
+    date: '2026-05-29',
+    title: '京津冀高校科研院所创新联盟成立暨创新基地揭牌',
+    source: '人民网',
+    url: 'https://bj.people.com.cn/n2/2026/0531/c14540-41596641.html',
+    score: 12,
+    eventId: 'E00360'
+  },
+  {
+    date: '2026-07-23',
+    title: '北京邮电大学空天科学与技术学院在雄安揭牌成立',
+    source: '河北省人民政府',
+    url: 'http://www.hebei.gov.cn/columns/580d0301-2e0b-4152-9dd1-7d7f4e0f4980/202607/25/5292a15f-66cb-4fc2-a16d-3a1df23bd1a4.html',
+    score: 11,
+    eventId: 'E00376'
+  },
+  {
+    date: '2026-03-24',
+    title: '京津冀12所学校签订科技教育合作协议',
+    source: '天津市教育委员会',
+    url: 'https://jy.tj.gov.cn/JYXW/TJJY/202603/t20260326_7270082.html',
+    score: 10,
+    eventId: 'E00325'
+  },
+  {
+    date: '2026-05-24',
+    title: '京津冀三地构建基础研究协同创新机制',
+    source: '北京市人民政府',
+    url: 'https://www.beijing.gov.cn/ywdt/gzdt/202605/t20260524_4663704.html',
+    score: 10,
+    eventId: 'E00358'
+  }
+];
